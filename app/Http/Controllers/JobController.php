3ee09d4bc27499d0a9c15391bed306f7049ea294
Malik001-G <adisamalikng0@gmail.com>
2024-11-18 11:51:22 +0100

    Setting the layout

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
@@ -12,7 +13,7 @@ class JobController extends Controller
      */
     public function index()
     {
-        //
+        return view('job.index', ['jobs' => Job::all()]);
     }
 
     /**

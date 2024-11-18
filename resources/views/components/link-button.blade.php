d74d1ceefbb002e88b9422f1faf4c26502a97ad0
Malik001-G <adisamalikng0@gmail.com>
2024-11-18 13:49:23 +0100

    show page- For each job

---
+++ resources/views/components/link-button.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ $href}}"
+    class="rounded-md border border-slate-300 text-sm bg-white text-black shadow-sm font-semibold px-2.5 py-1.5 text-center hover:bg-slate-100 transition-all duration-500 ease-linear">
+    {{$slot}}
+</a>

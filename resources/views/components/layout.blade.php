896559356a52157faa4ef45d3be7989500f9e9cb
Malik001-G <adisamalikng0@gmail.com>
2024-11-20 00:22:19 +0100

    cleaning up the styles

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,13 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel Job Board</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
-<div>
-    {{$slot}}
-</div>
+<body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-teal-100 to-blue-100 text-slate-700">
+    <div>
+        {{$slot}}
+    </div>
 </body>
 
 </html>

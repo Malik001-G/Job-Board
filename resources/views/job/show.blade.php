87c3700ec4d9297092a6534788a2b3893ef76511
Malik001-G <adisamalikng0@gmail.com>
2024-11-21 16:52:32 +0100

    added other jobs created by an employer

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -2,6 +2,31 @@
     <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index'), $job->title => '#']" />
     <x-job-card :job="$job">
         <p class="text-sm mb-4 text-slate-500">{!! nl2br(e($job->description)) !!}</p>
-
     </x-job-card>
+
+    <x-card class="mb-4">
+        <h2 class="mb-4 text-lg font-medium capitalize">
+            More {{$job->employer->company_name}} jobs
+        </h2>
+
+        <div class="text-sm text-slate-500">
+            @foreach ($job->employer->jobs as $otherJob)
+            <div class="mb-4 flex justify-between">
+                <div>
+                    <div class="text-slate-700">
+                        <a href="{{route('jobs.show', $otherJob)}}">
+                            {{$otherJob->title}}
+                        </a>
+                    </div>
+                    <div class="text-xs">
+                        {{$otherJob->created_at->diffForHumans()}}
+                    </div>
+                </div>
+                <div class="text-xs">
+                    ${{number_format($otherJob->salary)}}
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </x-card>
 </x-layout>
